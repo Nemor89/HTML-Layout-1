cdc8226a41e3e23ffbd10716b8febdc39f8c5c0c
Nemor89 <express-don@mail.ru>
2020-06-09 15:36:20 +0300

    index page version 0.1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <div>
           <img class ="logo" src="img/php-1.svg">
         </div>
-        <div class="description">
+        <div class="descriptionhead">
           <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
         </div>
         <div>
@@ -23,6 +23,51 @@
    </header>
    <main>
      <div class="content">
+      <div class="leftmenue">
+        <ul class="menu">
+          <li class="left"><a id ="left" href = "#">Категории</a></li>
+          <li class="left"><a id ="left" href = "#">Популярное</a></li>
+          <li class="left"><a id ="left" href = "#">Новое</a></li>
+          <li class="left"><a id ="left" href = "#">Блабла</a></li>
+          <li class="left"><a id ="left" href = "#">Блабла</a></li>
+          <li class="left"><a id ="left" href = "#">Блабла</a></li>
+        </ul>
+        <div class="leftblock"><textarea id="leftblock">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea></div>
+      </div>
+      <div class="blogs">
+        <div class="blog">
+          <p class="blogtext">Дата добавления: 2 000 лет до нашей эры</p>
+          <p class="blogtext">Автор: Профессионал</p>
+          <img class="blogimg" src="img/energy.jpg">
+          <p class="description">Как выпить 6 энергетиков подряд и не сдохнуть? Ответ профессионала</p>
+          <a id="read" href="#">Читать</a>
+        </div>
+        <div class="blog">
+          <p class="blogtext">Дата добавления: 09.06.2020 14:54</p>
+          <p class="blogtext">Автор: Nemor</p>
+          <img class="blogimg" src="img/grust.jpg">
+          <p class="description">Кати разрушают судьбы</p>
+          <a id="read" href="#">Читать</a>
+        </div>
+        <div class="blog">
+          <p class="blogtext">Дата добавления:</p>
+          <p class="blogtext">Автор</p>
+          <img class="blogimg" src="img/sosiska.jpg">
+          <p class="description">Ебанашка...</p>
+          <a id="read" href="#">Читать</a>
+        </div>
+        <div class="blog">
+          <p class="blogtext">Дата добавления:</p>
+          <p class="blogtext">Автор</p>
+          <img class="blogimg" src="img/zasekundu.jpg">
+          <p class="description">За секунду до...</p>
+          <a id="read" href="#">Читать</a>
+        </div>
+        <div>
+          <a id ="pages" href="#">Назад</a>
+          <a id ="pages" href="#">Вперед</a>
+        </div>
+      </div>
      </div>
    </main>
    <footer>
