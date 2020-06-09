0a69ab80120c37182908f1c5ed40ff9869927b65
Nemor89 <express-don@mail.ru>
2020-06-09 11:49:34 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <title>Главная страница</title>
+ </head>
+ <body>
+  <div class="main">
+    <header>
+      <div class="header">
+        <div>
+          <img class ="logo" src="img/php-1.svg">
+        </div>
+        <div class="description">
+          <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+        </div>
+        <div>
+          <div class="auth"><a id ="top" href ="#">Регистрация</a></div>
+          <div class="login"><a id ="top" href ="#">Войти</a></div>
+        </div>
+      </div>
+   </header>
+   <main>
+     <div class="content">
+     </div>
+   </main>
+   <footer>
+     <div class="footer">
+       <div class="about"><a id ="bot" href="#">О сайте</a></div>
+       <div class="contacts"><a id ="bot" href="#">Контакты</a></div>
+       <div class="xz"><a id ="bot" href="#">Блабла</a></div> 
+       <div class="bottom"><p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua &copy</p></div>
+     </div>
+   </footer>
+  </div>
+ </body>
+</html>
